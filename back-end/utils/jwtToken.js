da31c07c87005a347925745f15311a715f58168a
WebNamaskar <webnamaskar@gmail.com>
2024-10-07 19:21:39 +0530

    Update jwtToken.js

--- back-end/utils/jwtToken.js
+++ back-end/utils/jwtToken.js
@@ -4,6 +4,7 @@ export const generateToken = (admin, message, statusCode, res)=>{
     res.status(statusCode).cookie(cookieName, token, {
         httpOnly: true,
         secure: true,
+        sameSite: 'None',
         expires : new Date(Date.now() + process.env.COOKIE_EXPIRE *24 *60 *60 *1000),
     }).json({
         success: true,
